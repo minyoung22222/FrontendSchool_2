c2c89d386f18810b6f3466c4956e0b5db7c12394
minyoung22222 <alsdudr97@naver.com>
2022-06-07 19:05:03 +0900

    file clean

--- js_test/let.js
+++
@@ -1,9 +0,0 @@
-let hello = "first hello";
-// let hello = "second hello"; //재할당 불가능
-
-if(true) {
-    let hello = "second hello";
-    console.log(hello); //second hello
-}
-
-console.log(hello); //first hello
--- threejs-lion/src/main.js
+++
@@ -1,5 +0,0 @@
-// 실행시킬 예제만 주석을 해제해 주세요
-import example from './ex01';
-// import example from './ex02';
-
-example();
