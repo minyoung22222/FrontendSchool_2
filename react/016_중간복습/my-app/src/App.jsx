029dd5f0b9aefc50fb09c718ff09d740590fbfa3
minyoung22222 <alsdudr97@naver.com>
2022-06-13 23:04:12 +0900

    react 0613

---
+++ react/016_중간복습/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+// import { data } from './asset/data/data'
+import HomePage from './pages/HomePage/HomePage'
+import './reset.css'
+
+
+function App() {
+    // console.log(data)
+    return (
+        <>
+            <HomePage />   
+        </>
+    );
+}
+
+export default App;
